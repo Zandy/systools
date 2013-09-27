7981a109eed9bc450f3b87d3dd627af67797899d
Zandy <zandyo@gmail.com>
2013-09-27 21:46:54 +0800

    init repository; add changehostname.sh

---
+++ changehostname.sh
@@ -0,0 +1,16 @@
+#! /bin/bash
+:<<EOF
+By Zandy
+EOF
+
+oldhn=debian7-base
+newhn=debian7-data
+cf=$(grep -rn $oldhn /etc/|awk -F':' '{print $1}')
+for f in $cf; do
+        if [ -e $f ]; then
+                echo $f
+		sed -i "s:$oldhn:$newhn:g" $f
+        fi
+done
+hostname -v $newhn
+sysctl kernel.hostname=$newhn
